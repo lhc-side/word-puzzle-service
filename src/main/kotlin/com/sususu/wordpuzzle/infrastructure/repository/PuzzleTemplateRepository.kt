6fe237cfac5f781996edf90c937c2113a9e1f2e9
sungsan <kunga077@gmail.com>
2024-11-23 22:26:41 +0900

    feat: 랜덤 template 찾는 mock 쿼리 추가

--- src/main/kotlin/com/sususu/wordpuzzle/infrastructure/repository/PuzzleTemplateRepository.kt
+++ src/main/kotlin/com/sususu/wordpuzzle/infrastructure/repository/PuzzleTemplateRepository.kt
@@ -3,6 +3,4 @@ package com.sususu.wordpuzzle.infrastructure.repository
 import com.sususu.wordpuzzle.infrastructure.PuzzleTemplate
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PuzzleTemplateRepository : JpaRepository<PuzzleTemplate, Long>, PuzzleTemplateRepositoryCustom {
-
-}
+interface PuzzleTemplateRepository : JpaRepository<PuzzleTemplate, Long>, PuzzleTemplateRepositoryCustom
